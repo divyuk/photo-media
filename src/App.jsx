4488401d17ecd99502e29a9e2d838943983d4884
divyuk <div14kes@gmail.com>
2023-09-17 20:49:57 +0530

    Initial Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import UserList from "./components/UserList";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="container mx-auto">
+      <UserList />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { userReducer } from "./slice/UserSlice";
+
+export const store = configureStore({
+  reducer: {
+    users: userReducer,
+  },
+});
